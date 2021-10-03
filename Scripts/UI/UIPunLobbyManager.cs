d243afbd7697898df8df7c60db5ed4bb63a97c56
Insthync <ittipon.bay@gmail.com>
2021-10-04 06:49:05 +0700

    Implement connecting events

--- Scripts/UI/UIPunLobbyManager.cs
+++ Scripts/UI/UIPunLobbyManager.cs
@@ -6,11 +6,15 @@ namespace PunArena.UI
     {
         public UIPunRoomList uiRoomList;
         public UIPunRoom uiRoom;
+        public GameObject preloader;
 
         private void Start()
         {
             uiRoom.Hide();
             uiRoomList.Show();
+            PunArenaManager.Instance.onConnecting.AddListener(OnConnecting);
+            PunArenaManager.Instance.onConnect.AddListener(OnConnect);
+            PunArenaManager.Instance.onJoiningRoom.AddListener(OnJoining);
             PunArenaManager.Instance.onJoinRoom.AddListener(OnJoin);
             PunArenaManager.Instance.onLeaveRoom.AddListener(OnLeave);
             PunArenaManager.Instance.ConnectToBestCloudServer();
@@ -22,8 +26,28 @@ namespace PunArena.UI
             PunArenaManager.Instance.onLeaveRoom.RemoveListener(OnLeave);
         }
 
+        private void OnConnecting()
+        {
+            if (preloader)
+                preloader.SetActive(true);
+        }
+
+        private void OnConnect()
+        {
+            if (preloader)
+                preloader.SetActive(false);
+        }
+
+        private void OnJoining()
+        {
+            if (preloader)
+                preloader.SetActive(true);
+        }
+
         private void OnJoin()
         {
+            if (preloader)
+                preloader.SetActive(false);
             uiRoomList.Hide();
             uiRoom.Show();
         }
