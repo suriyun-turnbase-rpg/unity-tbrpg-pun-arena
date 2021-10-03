943d1f293a462ec8f18dda41a0d69af212c9b3e5
Insthync <ittipon.bay@gmail.com>
2021-10-04 06:12:25 +0700

    Add decision waiting UI

---
+++ Scripts/UI/UIPlayerDecisionWaiting.cs
@@ -0,0 +1,61 @@
+﻿using RealtimeArena.Battle;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PunArena.UI
+{
+    [RequireComponent(typeof(UICharacterStatsGeneric))]
+    public class UIPlayerDecisionWaiting : MonoBehaviour
+    {
+        public GameObject rootContainer;
+        public Text textCountDown;
+        public Image imageCountDownGage;
+        private UICharacterStatsGeneric uiCharacterStats;
+        private RealtimeArenaGameplayManager manager;
+        private bool dirtyIsActiveCharacter;
+        private float countDown;
+
+        private void Start()
+        {
+            uiCharacterStats = GetComponent<UICharacterStatsGeneric>();
+            manager = FindObjectOfType<RealtimeArenaGameplayManager>();
+            StartCountDown();
+        }
+
+        private void Update()
+        {
+            if (!uiCharacterStats.character)
+            {
+                if (rootContainer)
+                    rootContainer.SetActive(false);
+                return;
+            }
+
+            if (dirtyIsActiveCharacter != (uiCharacterStats.character.IsActiveCharacter && manager.WaitingForAction))
+            {
+                dirtyIsActiveCharacter = (uiCharacterStats.character.IsActiveCharacter && manager.WaitingForAction);
+                StartCountDown();
+            }
+
+            countDown -= Time.deltaTime;
+            if (countDown <= 0f)
+                countDown = 0f;
+
+            var rate = countDown / manager.decisionWaitingDuration;
+
+            if (textCountDown != null)
+                textCountDown.text = countDown <= 0 ? "" : countDown.ToString("N0");
+
+            if (imageCountDownGage != null)
+                imageCountDownGage.fillAmount = rate;
+
+            if (rootContainer)
+                rootContainer.SetActive(uiCharacterStats.character.IsActiveCharacter && countDown > 0f && manager.WaitingForAction);
+        }
+
+        public void StartCountDown()
+        {
+            countDown = manager.decisionWaitingDuration;
+        }
+    }
+}
