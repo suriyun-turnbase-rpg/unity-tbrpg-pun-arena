91ca0972a307de49a59f7359a5e275ac6e1eca0a
Insthync <ittipon.bay@gmail.com>
2021-10-04 06:12:54 +0700

    Add consts

---
+++ Scripts/PunArenaConsts.cs
@@ -0,0 +1,8 @@
+﻿namespace PunArena
+{
+    public static class PunArenaConsts
+    {
+        public const int MAX_PASSWORD_LENGTH = 6;
+        public const int ENTER_GAME_COUNT_DOWN = 5;
+    }
+}
