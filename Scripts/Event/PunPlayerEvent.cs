8724217e687fd65a1d9fd3a16ae8f7f063925336
Insthync <ittipon.bay@gmail.com>
2021-10-04 07:14:28 +0700

    Add pun player event

---
+++ Scripts/Event/PunPlayerEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine.Events;
+using PunPlayer = Photon.Realtime.Player;
+
+namespace PunArena.Event
+{
+    /// <summary>
+    /// [PunPlayer player]
+    /// </summary>
+    [Serializable]
+    public class PunPlayerEvent : UnityEvent<PunPlayer>
+    {
+    }
+}
